5a294964f0ddd42d56b9e799b907b110cd846caf
LiChao <lichao19870617@gmail.com>
2020-04-25 18:30:39 +0800

    move SSD_classification to hear

--- Workstation/SZ_classification/Visulization/lc_line.m
+++
@@ -1,8 +0,0 @@
-function lc_line(sepIndex, nNode, linewidth)
-% nNode: node¸öÊý
-n_net = length(sepIndex);
-for i=1:n_net
-    line([sepIndex(i),sepIndex(i)],[-10,nNode],'color','k','LineWidth',linewidth)
-    line([0, nNode],[sepIndex(i),sepIndex(i)],'color','k','LineWidth',linewidth)
-end
-end
