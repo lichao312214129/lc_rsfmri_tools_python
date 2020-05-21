6f33403b683722a93a32c963b8e96a0ee08c73cb
LiChao <lichao19870617@gmail.com>
2020-05-21 22:54:20 +0800

    Update lc_indentify_repeat_subjects.py

--- Utils/lc_indentify_repeat_subjects.py
+++ Utils/lc_indentify_repeat_subjects.py
@@ -4,9 +4,8 @@ Created on Thu Sep  6 18:54:37 2018
 找到与subjects重复的ID
 @author: lenovo
 """
-#
+
 import pandas as pd
-from selectSubjID_inScale import loadExcel
 from selectSubjID_inScale import selMain
 
 
@@ -14,7 +13,7 @@ def indentify_repeat_subjects_pairs(file, uid_header):
     """
     Identify the unique ID of subjects that have repeated scan or visit
     """
-    allClinicalData = loadExcel(file)
+    allClinicalData = pd.read_excel(file)
     originSubj = allClinicalData[uid_header]
     folder, basic, hamd17, hama, yars, bprs, logicIndex_scale, logicIndex_repeat = selMain(file)
     dia = allClinicalData[logicIndex_repeat]['诊断备注']
@@ -23,13 +22,12 @@ def indentify_repeat_subjects_pairs(file, uid_header):
     return repeatNote, repeatSubj
 
 
-#
+
 if __name__ == '__main__':
     repeatNote, repeatSubj = indentify_repeat_subjects_pairs(
-        file=r'D:\WorkStation_2018\WorkStation_CNN_Schizo\Scale\10-24大表.xlsx',
+        file=r'D:\WorkStation_2018\Workstation_2020_transdiagnositic_subtyping\10-24大表.xlsx',
         uid_header='folder')
     repeatPairs = pd.concat([repeatSubj, repeatNote], axis=1)
-    # 转格式
     repeatPairs = repeatPairs.astype({'folder': 'int'})
     print(repeatPairs)
-#    repeatPairs['诊断备注'] = repeatPairs.诊断备注.map(lambda x:float(x))
+    # repeatPairs['诊断备注'] = repeatPairs.诊断备注.map(lambda x:float(x))
